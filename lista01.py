b21d1f580c98003a2dca3b81680a099758a9a4e7
EC2 Default User <ec2-user@ip-172-31-33-66.ec2.internal>
2023-03-16 01:23:20 +0000

    listapython

---
+++ lista01.py
@@ -0,0 +1,2 @@
+numeros = [10,5,7,2,1]
+print (numeros[4])#Numeros são da esquerda para a direita, começando pelo 0
---
+++ lista02.py
@@ -0,0 +1,8 @@
+# Crinado uma lista
+minha_lista = [1,2,3,4,5,9]
+
+#usando a função len() para contar elementos da lista
+
+tamanho_lista = len(minha_lista)
+
+print("O tamando da lista é:", tamanho_lista)
